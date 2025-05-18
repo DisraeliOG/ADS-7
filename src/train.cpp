4e9e6d0d7345ef4a4f79e85df784084d0eb70e7c
DisraeliOG <118463303+DisraeliOG@users.noreply.github.com>
2025-05-18 19:45:01 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -3,58 +3,33 @@
 
 Train::Train() : first(nullptr), countOp(0) {}
 
-Train::~Train() {
-  if (!first) return;
-
-  Car* cur = first->next;
-  while (cur != first) {
-    Car* next = cur->next;
-    delete cur;
-    cur = next;
-  }
-  delete first;
-}
-
 void Train::addCar(bool light) {
-  Car* new_car = new Car{light, nullptr, nullptr};
+  Car* node = new Car;
+  node->light = light;
   if (!first) {
-    new_car->next = new_car;
-    new_car->prev = new_car;
-    first = new_car;
+    node->next = node;
+    node->prev = node;
+    first = node;
   } else {
-    Car* tail = first->prev;
-    tail->next = new_car;
-    new_car->prev = tail;
-    new_car->next = first;
-    first->prev = new_car;
+    Car* last = first->prev;
+    last->next = node;
+    node->prev = last;
+    node->next = first;
+    first->prev = node;
   }
 }
 
 int Train::getLength() {
   if (!first) return 0;
-
+  int length = 1;
   countOp = 0;
+
   Car* cur = first;
-
-  cur->light = true;
-
-  for (int k = 1;; ++k) {
-    Car* probe = cur;
-    for (int i = 0; i < k; ++i) {
-      probe = probe->next;
-      ++countOp;
-    }
-
-    if (probe->light) {
-      return k;
-    } else {
-      probe->light = true;
-      for (int i = 0; i < k; ++i) {
-        probe = probe->prev;
-        ++countOp;
-      }
-    }
+  while ((cur = cur->next) != first) {
+    ++length;
+    ++countOp;
   }
+  return length;
 }
 
 int Train::getOpCount() {
