beadbda670b05a82bfb2cc99811510b35e716dfc
DisraeliOG <118463303+DisraeliOG@users.noreply.github.com>
2025-05-18 19:49:54 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,49 +1,48 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
 
-Train::Train() : first(nullptr), countOp(0) {}
+Train::Train() : countOp(0), first(nullptr) {}
 
 void Train::addCar(bool light) {
-  Car* node = new Car;
-  node->light = light;
-  if (!first) {
-    node->next = node;
-    node->prev = node;
-    first = node;
-  } else {
-    Car* last = first->prev;
-    last->next = node;
-    node->prev = last;
-    node->next = first;
-    first->prev = node;
-  }
+    Car* newCar = new Car{light, nullptr, nullptr};
+    if (!first) {
+        newCar->next = newCar;
+        newCar->prev = newCar;
+        first = newCar;
+    } else {
+        Car* last = first->prev;
+        last->next = newCar;
+        newCar->prev = last;
+        newCar->next = first;
+        first->prev = newCar;
+    }
 }
 
 int Train::getLength() {
-  if (!first) return 0;
-  int length = 1;
-  countOp = 0;
+    if (!first) {
+        countOp = 0;
+        return 0;
+    }
 
-  Car* cur = first;
+    int length = 1;
+    countOp = 0;
 
-  cur = cur->next;
-  ++countOp;
-  while (cur != first) {
-    cur = cur->next;
-    ++length;
-    ++countOp;
-  }
+    Car* cur = first;
+    while (cur->next != first) {
+        cur = cur->next;
+        ++length;
+        ++countOp;
+    }
 
-  cur = first->prev;
-  ++countOp;
-  while (cur != first) {
-    cur = cur->prev;
-    ++countOp;
-  }
+    cur = first;
+    while (cur->prev != first) {
+        cur = cur->prev;
+        ++countOp;
+    }
 
-  return length;
+    return length;
 }
 
 int Train::getOpCount() {
-  return countOp;
+    return countOp;
 }
